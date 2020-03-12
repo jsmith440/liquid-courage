1d28a4278a84a62f7eecdc751a0e0be9ac252acd
jsmith440 <jsmith440@cnm.edu>
2020-03-12 08:09:27 -0600

    Started packet manager...but pretty sure this isnot the correct way. I will investigate more after test.

--- app/src/main/java/edu/cnm/deepdive/liquidcourage/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/liquidcourage/controller/MainActivity.java
@@ -2,18 +2,40 @@ package edu.cnm.deepdive.liquidcourage.controller;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 import edu.cnm.deepdive.liquidcourage.R;
+import edu.cnm.deepdive.liquidcourage.view.AppRecyclerAdapter;
+import edu.cnm.deepdive.liquidcourage.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+/*
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.main_activity);
+    setContentView(R.layout.activity_main);
     if (savedInstanceState == null) {
       getSupportFragmentManager().beginTransaction()
           .replace(R.id.container, MainFragment.newInstance())
           .commitNow();
     }
   }
+*/
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    RecyclerView iconList = findViewById(R.id.icon_list);
+    MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+    viewModel.getApps().observe(this, (apps) -> {
+      AppRecyclerAdapter adapter = new AppRecyclerAdapter(this, apps);
+      iconList.setAdapter(adapter);
+    });
+    viewModel.refreshApps();
+  }
+
+
+
 }
--- app/src/main/java/edu/cnm/deepdive/liquidcourage/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/liquidcourage/viewmodel/MainViewModel.java
@@ -1,26 +1,50 @@
 package edu.cnm.deepdive.liquidcourage.viewmodel;
 
 import android.app.Application;
-import androidx.lifecycle.ViewModel;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.Lifecycle.Event;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.OnLifecycleEvent;
-import edu.cnm.deepdive.liquidcourage.model.entity.App;
-import edu.cnm.deepdive.liquidcourage.model.repository.LiquidCourageRepository;
-import edu.cnm.deepdive.liquidcourage.service.LiquidCourageService;
-import io.reactivex.android.schedulers.AndroidSchedulers;
+import edu.cnm.deepdive.liquidcourage.model.AppInfo;
+import edu.cnm.deepdive.liquidcourage.model.repository.AppInfoRepository;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import java.text.ParseException;
-import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+public class MainViewModel extends AndroidViewModel implements LifecycleObserver {
 
-public class MainViewModel extends ViewModel {
-  // TODO: Implement the ViewModel
+  private final AppInfoRepository repository;
+  private final MutableLiveData<List<AppInfo>> apps;
+  private final MutableLiveData<Throwable> throwable;
+  private final CompositeDisposable pending;
+
+  public MainViewModel(@NonNull Application application) {
+    super(application);
+    repository = new AppInfoRepository(application);
+    apps = new MutableLiveData<>();
+    throwable = new MutableLiveData<>();
+    pending = new CompositeDisposable();
+  }
+
+  public LiveData<List<AppInfo>> getApps() {
+    return apps;
+  }
+
+  public void refreshApps() {
+    pending.add(
+        repository.getPackages()
+            .subscribe(
+                apps::postValue,
+                throwable::postValue
+            )
+    );
+  }
+
+  @OnLifecycleEvent(Event.ON_STOP)
+  private void clearPending() {
+    pending.clear();
+  }
+
 }
